fd6aa3b4f4070174c2082364acf769318ec866ef
windasrk <windasrk@gmail.com>
2020-08-15 09:40:43 +0700

    komit

--- resources/views/pertanyaan/create.blade.php
+++ resources/views/pertanyaan/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('adminlte.master')
 
 @section('title', 'Buat Pertanyaan')
 
@@ -15,9 +15,9 @@ $(function() {
 });
 </script>
 <div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <h1>Buat Pertanyaan</h1>
+ 
+    <div class="col-md-12 ml-3 mr-3 mt-3">
+      <h2>Create a new question</h2>
 
       <div class="card">
         <div class="card-body">
@@ -28,28 +28,31 @@ $(function() {
             @csrf
 
             <div class="form-group">
-              <label>Judul</label>
+              <label>Title</label>
               <input type="text" class="form-control" name="title">
             </div>
 
             <div class="form-group">
-            <label class="col-form-label">Keterangan</label>
+            <label class="col-form-label">Description</label>
             <textarea class="ckeditor" id="content" name="content"></textarea>
             </div>
 
             <div class="form-group">
-              <label>Tag</label>
+              <label>Tags</label>
               <input type="text" class="form-control" name="tag">
-              <small class="form-text text-muted">Pisahkan tag dengan koma</small>
+              <small class="form-text text-muted">Seperate tags with commas (ex: html, php)</small>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
-            <a class="btn btn-outline-primary" href="{{ route('pertanyaan.index') }}">Kembali</a>
-          </form>
+           
+           <br>
+            <a class="btn btn-outline-primary" href="{{ route('pertanyaan.index') }}">Back</a>&nbsp&nbsp
+
+            <button type="submit" class="btn btn-primary">Submit</button>&nbsp&nbsp       
+               </form>
         </div>
       </div>
     </div>
-  </div>
+  
 </div>
 <script>
     $("#inputFile").change(function(event) {
